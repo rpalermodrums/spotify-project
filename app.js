c0dad0745bca717a906dc6bc62aab22fb09785e0
Ryan Palermo <stix250@gmail.com>
2017-04-13 09:06:42 -0400

    more setup

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ $(function() {
   $('#main-input').on('keyup', function(e) {
     $('#main-container').text($('#main-input').val());
   });
+  $('.form-main').on('submit', function(event) {
+    event.preventDefault();
+    const searchTerm = $('#main-input').val();
+
+    console.log(searchTerm);
+    $('#main-input').val('')
+  })
 });
